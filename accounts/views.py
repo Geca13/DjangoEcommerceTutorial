0fc32b3b448bd36789468400d063027b18c7da73
Geca13 <karafiloski84@gmail.com>
2021-10-25 08:20:26 +0200

    cart completed but it is disaster code provided by the instructor

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,20 @@
+from typing import TYPE_CHECKING
 from django.contrib import messages, auth
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from accounts.forms import RegistrationForm
 from accounts.models import Account
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+import requests
+
+
 
 def register(request):
     if request.method == 'POST':
@@ -17,8 +29,21 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'registration succesfull')
-            return redirect('register')
+
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user':user,
+                'domain': current_site, 
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)), 
+                'token': default_token_generator.make_token(user), 
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            # messages.success(request, 'Go to your email to activate your account')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
            
@@ -34,9 +59,55 @@ def login(request):
          user = auth.authenticate(email=email, password=password)
 
          if user is not None:
+             try:
+                 cart = Cart.objects.get(cart_id=_cart_id(request))
+                 cart_item_exists = CartItem.objects.filter( cart=cart).exists()
+                 if cart_item_exists:
+                     cart_item = CartItem.objects.filter(cart=cart)
+                     product_variation = []
+    
+                     for item in cart_item:
+                         variation = item.variations.all()
+                         product_variation.append(list(variation))
+                     
+                     cart_item = CartItem.objects.filter( user=user) 
+                     existing_variation_list = []
+                     cart_item_id = []
+
+                     for item in cart_item:
+                       existing_variation = item.variations.all()
+                       existing_variation_list.append(list(existing_variation))
+                       cart_item_id.append(item.id)
+
+                     for pr in product_variation:
+                         if pr in existing_variation_list:
+                              index = existing_variation_list.index(pr) 
+                              item_id = id[index] 
+                              item = CartItem.objects.get(id=item_id)
+                              item.quantity +=1
+                              item.user = user
+                              item.save()
+                         else:
+                             cart_item = CartItem.objects.filter(cart=cart)
+                             
+                             for item in cart_item:
+                                 item.user = user
+                                 item.save()
+             except:
+                 pass    
              auth.login(request, user)
              messages.success(request, 'You logged in succesfully')
-             #return redirect('home') 
+             url = request.META.get('HTTP_REFERER')
+             try:
+                 query = requests.utils.urlparse(url).query
+
+                 params = dict(x.split('=') for x in query.split('&'))
+                 if 'next' in params:
+                     nextPage = params['next']
+                     return redirect(nextPage)
+             except:
+                 return redirect('dashboard')   
+             
          else:
              messages.error(request, 'Please check your credentials and try again')
              return redirect('login')
@@ -48,3 +119,86 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You logout succesfully')
     return redirect('home')
+
+
+def activate(request,uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'You verified your account succesfully')
+        return redirect('login')  
+    else: 
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')  
+
+@login_required(login_url = 'login')
+def dashboard(request):
+
+    return render(request, 'accounts/dashboard.html')      
+
+   
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+
+            current_site = get_current_site(request)
+            mail_subject = 'Reset your password'
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user':user,
+                'domain': current_site, 
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)), 
+                'token': default_token_generator.make_token(user), 
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            messages.success(request, 'Email with link to reset your password has been sent to your email address')
+            return redirect('login')
+
+        else:
+            messages.error(request, 'User with that email does not exist')
+            return redirect('forgetPassword')
+
+    return render(request, 'accounts/forgotPassword.html')   
+
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'This link is expired')
+        return redirect('login')
+
+    
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+           uid = request.session.get('uid')
+           user = Account.objects.get(pk=uid)
+           user.set_password(password)
+           user.save()
+           messages.success(request, 'You have successfully changed your password')
+           return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')    
--- gecacart/settings.py
+++ gecacart/settings.py
@@ -142,3 +142,12 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'javageca@gmail.com'
+EMAIL_HOST_PASSWORD = 'Vardar13Negotino'
+
